dc7037b66d6d28a0398b8c3844fbc5e8c2581ca1
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-19 17:58:43 +0100

    Dernier commit

--- src/main/java/champollion/Intervention.java
+++ src/main/java/champollion/Intervention.java
@@ -16,10 +16,17 @@ public class Intervention {
     private Date debut;
     private int duree;
     private boolean annulee = false;
+    private Salle s;
+    private UE u;
+    private Enseignant e;
+    
     
     public Intervention(Salle s, UE u, Enseignant e, Date d, int duree){
         this.debut = d;
         this.duree = duree;
+        this.s = s;
+        this.u = u;
+        this.e = e;
     }
 
     public Date getDebut() {
@@ -45,6 +52,33 @@ public class Intervention {
     public void setAnnulee(boolean annulee) {
         this.annulee = annulee;
     }
+
+    public Salle getS() {
+        return s;
+    }
+
+    public void setS(Salle s) {
+        this.s = s;
+    }
+
+    public UE getU() {
+        return u;
+    }
+
+    public void setU(UE u) {
+        this.u = u;
+    }
+
+    public Enseignant getE() {
+        return e;
+    }
+
+    public void setE(Enseignant e) {
+        this.e = e;
+    }
+    
+    
+    
     public void annulerIntervention(Intervention i,Enseignant e){
         i.setAnnulee(true);
         i.setDebut(null);

26d6dba55a5b06fecb835cd6a31fe500a43bef50
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-19 16:35:57 +0100

    Ajout de tests

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -1,17 +1,22 @@
 package champollion;
 
+import java.util.Date;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ChampollionJUnitTest {
 	Enseignant untel;
 	UE uml, java;
+        Salle s;
+        Date date;
 		
 	@BeforeEach
 	public void setUp() {
 		untel = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
-		java = new UE("Programmation en java");		
+		java = new UE("Programmation en java");
+                s = new Salle("A01",30);
+                date = new Date(2020,11,19);
 	}
 	
 
@@ -35,6 +40,38 @@ public class ChampollionJUnitTest {
 		assertEquals(10 + 20, untel.heuresPrevuesPourUE(uml),
                          "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
 		
+                // 10h de CM = 15h de TD
+                untel.ajouteEnseignement(uml, 10, 0, 0);
+                
+		assertEquals(30+15, untel.heuresPrevuesPourUE(uml),
+                         "L'enseignant doit maintenant avoir 45 heures prévues pour l'UE 'uml'");		
+                
+                // 20h de TP = 15h de TD
+                untel.ajouteEnseignement(uml, 0, 0, 20);
+                
+		assertEquals(45+15, untel.heuresPrevuesPourUE(uml),
+                         "L'enseignant doit maintenant avoir 60 heures prévues pour l'UE 'uml'");		
 	}
-	
+        
+        
+        @Test
+        public void testSousService(){
+            untel.ajouteEnseignement(uml, 10, 10, 10);
+            assertTrue(untel.enSousService(),"L'enseignant devrait être en sous-service.");
+        }
+	@Test
+        public void testHeurePlannifiée(){
+            Intervention i = new Intervention(s, uml,untel,date,3);
+            untel.ajouterIntervention(i);
+            assertEquals(3,untel.heurePlanifiees(),"Le cours aurait dû durer 3h.");
+
+        }
+        
+        @Test 
+        public void annultationCours(){
+            Intervention i = new Intervention(s, uml,untel,date,3);
+            untel.ajouterIntervention(i);
+            i.annulerIntervention(i,untel);
+            assertEquals(0, untel.heurePlanifiees(),"Le cours aurait dû s'annuler");            
+        }
 }
